4b86e7440016f146e91245e87fcdcb2cdab850d9
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-04-29 18:04:04 +0100

    take two

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Serialize};
+use reqwest::header::USER_AGENT;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct ComicVineResponse {
@@ -11,7 +12,7 @@ struct ComicVineResponse {
     results: Vec<Publisher>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 struct Publisher {
     id: i32,
     api_detail_url: String,
@@ -21,19 +22,29 @@ struct Publisher {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv_vault::dotenv();
+    let _ = dotenv_vault::dotenv();
     let url = "https://comicvine.gamespot.com/api/publisher/";
     let publisher_id = "4010-4212";
     let api_key = std::env::var("COMICVINE_KEY").expect("COMICVINE_KEY must be set");
     let query = "&format=json&sort=name:asc";
     let full_url = format!("{}{}/?api_key={}{}", url, publisher_id, api_key, query);
 
-    let response = reqwest::blocking::get(full_url)?;
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .get(full_url)
+        .header(
+            USER_AGENT,
+            "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+        )
+        .send()?;
 
     if response.status().is_success() {
+        println!("Request successful");
+        println!("Headers:\n{:#?}", response.headers());
+        println!("Body:\n{}", response.text()?);
         // Deserialize the JSON response into a Post struct
-        let post: ComicVineResponse = response.json()?;
-        println!("{:?}", post);
+        // let comic: ComicVineResponse = response.json()?;
+        // println!("{:?}", comic);
     } else {
         println!("Request failed with status code: {}", response.status());
     }
